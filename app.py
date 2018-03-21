1e720ebc852b4c996edbf202afda839fbe9ec043
localadmin <briz.andrew@gmail.com>
2018-03-21 17:42:33 -0400

    Added some comments and quality of life features.

--- app.py
+++ app.py
@@ -7,33 +7,76 @@ import os
 
 NEWS_API_TOKEN = os.environ.get('NEWS_API_TOKEN')
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+SLACK_APP_TOKEN = os.environ.get('SLACK_APP_TOKEN')
 SLACK_VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN')
+BOT_USER_ID = os.environ.get('BOT_USER_ID')
 
 newsapi = NewsApiClient(api_key=NEWS_API_TOKEN)
-sc = SlackClient(SLACK_BOT_TOKEN)
+sc_bot = SlackClient(SLACK_BOT_TOKEN)
+sc_app = SlackClient(SLACK_APP_TOKEN)
 
 app = Flask(__name__)
 
 
 def get_the_news(payload):
+    """
+    Uses the News API to get news stories based on a search query
+    """
     user_id = payload['user_id']
     channel_id = payload['channel_id']
 
+    # Make sure the bot is in the channel it's being called from
+    sc_app.api_call(
+        "conversations.invite",
+        channel=channel_id,
+        users=BOT_USER_ID
+    )
+
     search_term = payload['text']
+
     all_headlines = newsapi.get_top_headlines(q=search_term)
     top_headlines = all_headlines['articles'][:5]
 
-    messages = create_interactive_messages(top_headlines)
-    for message in messages:
-        sc.api_call(
-            "chat.postEphemeral",
-            channel=channel_id,
-            attachments=message,
-            user=user_id
-        )
+    # If there are top headlines, reply to the user
+    if len(top_headlines) > 0:
+        messages = create_interactive_messages(top_headlines)
+        for message in messages:
+            sc_bot.api_call(
+                "chat.postEphemeral",
+                channel=channel_id,
+                attachments=message,
+                user=user_id
+            )
+    # If there are not top headlines, try getting any headlines
+    else:
+
+        all_headlines = newsapi.get_everything(q=search_term, sort_by='relevancy')
+        top_headlines = all_headlines['articles'][:5]
+
+        # If there are any headlines, reply to the user
+        if len(top_headlines) > 0:
+            messages = create_interactive_messages(top_headlines)
+            for message in messages:
+                sc_bot.api_call(
+                    "chat.postEphemeral",
+                    channel=channel_id,
+                    attachments=message,
+                    user=user_id
+                )
+        # If there are no headlines, respond saying so
+        else:
+            sc_bot.api_call(
+                "chat.postEphemeral",
+                channel=channel_id,
+                text="No news found with the search `%s`" % search_term,
+                user=user_id
+            )
 
 
 def create_interactive_messages(headlines):
+    """
+    Creates attatchemtns for interactive messages with buttons to share and/or dismiss them.
+    """
     messages = []
     for headline in headlines:
         message = [
@@ -53,7 +96,7 @@ def create_interactive_messages(headlines):
                         "name": "share",
                         "text": "Share",
                         "type": "button",
-                        "value": json.dumps(headline),
+                        "value": json.dumps(headline),  # Used to recreate the message when pushed
                         "style": "primary"
                     },
                     {
@@ -72,6 +115,9 @@ def create_interactive_messages(headlines):
 
 
 def create_story_message(headline):
+    """
+    Creates attatchement for headline being shared
+    """
     return [
         {
             "title": headline["title"],
@@ -83,19 +129,33 @@ def create_story_message(headline):
     ]
 
 
+def authorize(token):
+    """
+    Verfiy the message is coming from Slack
+    """
+    if token == SLACK_VERIFICATION_TOKEN:
+        return True
+    else:
+        return False
+
+
 @app.route('/', methods=['GET'])
 def index():
     """
-    Confirm that the server is working.
+    Confirm that the server is working
     """
-    return ('This web app is working!')
+    return ('Working.')
 
 
 @app.route('/slack-slash', methods=['POST'])
 def slack_slash():
-    if request.form["token"] != SLACK_VERIFICATION_TOKEN:
+    """
+    Handles slash command webhooks being sent to this server
+    """
+    if not authorize(request.form["token"]):
         return (None, 403, None)
 
+    # Start getting and posting the news in a new thread
     thr = Thread(target=get_the_news, args=[request.form])
     thr.start()
 
@@ -104,10 +164,14 @@ def slack_slash():
 
 @app.route('/slack-button', methods=['POST'])
 def slack_button():
+    """
+    Handles interactive message webhooks being sent to this server
+    """
     payload = json.loads(request.form['payload'])
-    if payload["token"] != SLACK_VERIFICATION_TOKEN:
+    if not authorize(payload["token"]):
         return (None, 403, None)
 
+    # If the share button was pressed
     if payload["actions"][0]["name"] == "share":
         headline = json.loads(payload["actions"][0]["value"])
         message = create_story_message(headline)
@@ -115,13 +179,14 @@ def slack_button():
         channel_id = payload["channel"]["id"]
         user_id = payload["user"]["id"]
 
-        sc.api_call(
+        sc_bot.api_call(
             "chat.postMessage",
             text="<@%s> shared this:" % user_id,
             channel=channel_id,
             attachments=message,
         )
 
+    # Regardless, delete the message
     return jsonify({
         'delete_original': True
     })
@@ -129,4 +194,4 @@ def slack_button():
 
 if __name__ == '__main__':
     # Fire up the Flask test server
-    app.run(debug=False, use_reloader=True)
+    app.run(debug=True, use_reloader=True)
